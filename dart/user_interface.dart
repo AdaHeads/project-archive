07b4e52820238522a6bee4f8fb804d592a6734b0
Thomas Pedersen <tp@adaheads.com>
2012-11-28 10:48:26 +0100

    Tryed to make the Panels slide in and out.

--- dart/user_interface.dart
+++ dart/user_interface.dart
@@ -1,9 +1,11 @@
 library user_insterface;
 
-import 'model/call.dart';
 import 'dart:html';
 
 import 'package:rikulo/effect.dart';
+
+import 'log.dart';
+import 'model/call.dart';
 
 void Reload_Call_List(List<Call> Calls)
 {
@@ -18,26 +20,54 @@ void Reload_Call_List(List<Call> Calls)
 
 void Call_Panel_Show(){
   DivElement panel = query("#Call_Panel");
-  SlideInEffect.createAction(panel, panel.clientWidth,true, SlideDirection.SOUTH);
+  var fade = false;
+  var direction = SlideDirection.NORTH;
+  var effect = new SlideInEffect(panel, direction: direction, fade: fade);
+  effect.run();
+  print("Call_Panel_Show");
 }
 
 void Call_Panel_Hide(){
   DivElement panel = query("#Call_Panel");
-  SlideOutEffect.createAction(panel,true, SlideDirection.SOUTH);
+  //var motion = SlideOutEffect.createAction(panel,true, SlideDirection.NORTH);
+  //var se = new ShowEffect(panel, motion);
+  //se.run();
+  var fade = false;
+  var direction = SlideDirection.NORTH;
+  var effect = new SlideOutEffect(panel, direction: direction, fade: fade);
+  effect.run();
+  print("Call_Panel_Hide");
 }
 
 void Event_Panel_Show(){
-
+  DivElement panel = query("#Event_Panel");
+  var motion = SlideInEffect.createAction(panel, panel.clientWidth,true, SlideDirection.SOUTH);
+  var se = new ShowEffect(panel, motion);
+  se.run();
+  print("Event_Panel_Show");
+  Log.Message(Level.DEBUG, "Event_Panel_Show", "User_interface");
 }
 
 void Event_Panel_Hide(){
-
+  DivElement panel = query("#Event_Panel");
+  var motion = SlideOutEffect.createAction(panel,true, SlideDirection.SOUTH);
+  var se = new ShowEffect(panel, motion);
+  se.run();
+  print("Event_Panel_Hide");
 }
 
 void Statistics_Panel_Show(){
-
+  DivElement panel = query("#Statistics_Panel");
+  var motion = SlideInEffect.createAction(panel, panel.clientWidth,true, SlideDirection.SOUTH);
+  var se = new ShowEffect(panel, motion);
+  se.run();
+  print("Statistics_Panel_Show");
 }
 
 void Statistics_Panel_Hide(){
-
+  DivElement panel = query("#Statistics_Panel");
+  var motion = SlideOutEffect.createAction(panel,true, SlideDirection.SOUTH);
+  var se = new ShowEffect(panel, motion);
+  se.run();
+  print("Statistics_Panel_Hide");
 }
