e00e41d15710d27f9b08922824250a105d947095
Thomas Pedersen <tp@adaheads.com>
2012-11-28 10:50:03 +0100

    changed the log system to give more information, and be more configurable.

--- dart/configuration.dart
+++ dart/configuration.dart
@@ -2,9 +2,11 @@ part of bob;
 
 class Configuration {
   //static String WebSocket_Uri = "ws://192.168.2.146:4242/notifications";
-  static String WebSocket_Uri = "ws://localhost:4242/notifications";
+  static String WebSocket_URL = "ws://localhost:4242/notifications";
 
-  static String alice_URI = "http://alice.adaheads.com:4242/";
+  static String alice_URL = "http://localhost:4242/";
 
   static String SIP_Username = "softphone1";
+
+  static Level LogLevel = Level.DEBUG;
 }
--- dart/log.dart
+++ dart/log.dart
@@ -3,19 +3,39 @@ library log;
 import 'dart:html';
 import 'dart:collection';
 
+import '../bob.dart';
+
 class Level{
   final String _text;
+  final int _level;
+  const Level(this._text, this._level);
 
-  const Level(this._text);
+  static final Level DEBUG = const Level("DEBUG",0);
+  static final Level INFO = const Level("INFO",1);
+  static final Level ERROR = const Level("ERROR",2);
+  static final Level CRITICAL = const Level("CRITICAL",3);
 
-  static final Level DEBUG = const Level("DEBUG");
-  static final Level NOTE = const Level("NOTE");
-  static final Level WARNING = const Level("WARNING");
-  static final Level ERROR = const Level("ERROR");
+  bool operator >= (Level lvl){
+    return _level >= lvl._level;
+  }
+
+  bool operator < (Level lvl){
+    return _level < lvl._level;
+  }
 }
 
 class Log {
   static void Message(Level level,String text, [String filename = "no-filename"]){
-    print("[${level._text}] $filename - $text");
+    //Returns out, if the LogLevel is higher then the message.
+    if (level < Configuration.LogLevel){
+      return;
+    }
+
+    var time = new Date.now().toString();
+    var finalText = "$time [${level._text}] $filename - $text";
+
+    print(finalText);
+
+    query("#Event_Log").elements.add(new LIElement()..text = finalText);
   }
 }
