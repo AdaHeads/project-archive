057d39a803db5ee8afe76eda43d381d6c9e322a2
Thomas Pedersen <tp@adaheads.com>
2012-12-05 16:33:18 +0100

    Spread the licence.

--- bob.dart
+++ bob.dart
@@ -1,4 +1,4 @@
-//Copyright (C) 2012-, AdaHeads K/S - This is free software;  you can
+//Copyright (C) 2012-, AdaHeads K/S - This is free software; you can
 //redistribute it and/or modify it under terms of the
 //GNU General Public License  as published by the Free Software  Foundation;
 //either version 3,  or (at your  option) any later version. This library is
