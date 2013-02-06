301fcdb2b9732d7eefef3e5754c7c0809f167ca6
Thomas Løcke <tl@adaheads.com>
2013-02-06 09:41:18 +0100

    Further work on the Configuration library. Added JsonObject to pubspec.

--- src/dart/configuration.dart
+++ src/dart/configuration.dart
@@ -12,6 +12,7 @@
   see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
   <http://www.gnu.org/licenses/>.
 */
+
 /**
  * In this library we fetch the configuration for Bob.
  */
@@ -31,32 +32,29 @@ import 'logger.dart';
  * TODO: Write comment.
  */
 class Configuration {
-  static const CONFIGURATION_URL = 'http://alice.adaheads.com:4242/configuration';
   static Configuration _instance;
-
-//  Map _json;
-//  Map get asJson => _json;
+  static const String CONFIGURATION_URL = 'http://alice.adaheads.com:4242/configuration';
 
   bool _loaded = false;
   bool get loaded => _loaded;
 
   Level serverLogLevel = Level.OFF;
 
-  String standardGreeting = 'Velkommen til...';
-  bool enablePolling = false;
-  Uri sipPbx = new Uri('asterisk2.adaheads.com');
+  String standardGreeting;
+  bool enablePolling;
+  Uri sipPbx;
   int agentID = 1;
 
-  bool websocketReconnect = false;
-  int websocketInterval = 1000;
-  Uri websocketUri = new Uri('ws://alice.adaheads.com:4242/notifications');
+  bool websocketReconnect;
+  int websocketInterval;
+  Uri websocketUri;
 
-  Uri pjsuaHttpdUri = new Uri ('http://localhost:30200');
+  Uri pjsuaHttpdUri;
   String pjsuaPassword;
   String pjsuaUsername;
 
-  int systemConsoleMaxItems = 15;
-  int eventLogMaxItems = 20;
+  int systemConsoleMaxItems;
+  int eventLogMaxItems;
   Level logLevel = Level.ALL;
   Uri aliceUri = new Uri('http://alice.adaheads.com:4242/');
 
@@ -96,72 +94,26 @@ class Configuration {
     }
   }
 
-  // TODO when there comes more configurations that needs handling, do it here.
-  // TODO Validate and check for missing.
-
-  // --QUESTIONS-
-  // Why are there SIP_Username/Password and SIP_Account with almost the same information?
-  // Is it possible to valid an url?
-  // Would it make any sense to leave out some information? because then there is missing a Required bool.
-
-  // --REMAKR--
-  // Debug_Enabled Makes no sense to have, when there is a ServerLogLevel.
-  // I did not make it a shorthand if or Conditional as it's called, because it made very long lines, and i think it's more clear what is going on like this.
   void _parseConfiguration(Map json){
+    standardGreeting = _stringValue(json, 'Standard_Greeting', 'Velkommen til...');
+    enablePolling = _boolValue(json, 'Enable_Polling', false);
+    sipPbx = new Uri(_stringValue (json, 'SIP_PBX', 'asterisk2.adaheads.com'));
+    agentID = _intValue (json, 'Agent_ID', 0);
+
+    Map websocketMap = json['Websocket'];
+    websocketReconnect = _boolValue(websocketMap, 'Reconnect', false);
+    websocketInterval = _intValue(websocketMap, 'Reconnect_Interval', 1000);
+    websocketUri = new Uri(_stringValue(websocketMap, 'URI', 'ws://localhost:4242/notifications'));
+
+    pjsuaHttpdUri = new Uri(_stringValue(json, 'PJSUA_HTTPD_URI', 'http://localhost:30200'));
+
+    Map systemConsoleMap = json['System_Console'];
+    systemConsoleMaxItems = _intValue(systemConsoleMap, 'Max_Items', 15);
+
+    Map eventLogMap = json['Event_Log'];
+    eventLogMaxItems = _intValue(eventLogMap, 'Max_Items', 20);
+
     String currentNodeName = 'Standard_Greeting';
-    if (_validString(json, currentNodeName)) {
-      standardGreeting = json[currentNodeName];
-    }
-
-    currentNodeName = 'Enable_Polling';
-    if (_validBool(json, currentNodeName)) {
-      enablePolling = json[currentNodeName];
-    }
-
-    currentNodeName = 'SIP_PBX';
-    if (_validString(json, currentNodeName)) {
-      sipPbx = new Uri(json[currentNodeName]);
-    }
-
-    currentNodeName = 'Agent_ID';
-    if (_validInt(json, currentNodeName, min:0)) {
-      agentID = json[currentNodeName];
-    }
-
-    currentNodeName = 'Websocket';
-    if (_validMap(json, currentNodeName)){
-      var websocket = json[currentNodeName];
-
-      currentNodeName = 'Reconnect';
-      if (_validBool(websocket, currentNodeName)){
-        websocketReconnect = websocket[currentNodeName];
-      }
-
-      currentNodeName = 'Reconnect_Interval';
-      if (_validInt(websocket, currentNodeName, min: 0)){
-        websocketInterval = websocket[currentNodeName];
-      }
-
-      currentNodeName = 'URI';
-      if (_validString(websocket, currentNodeName)){
-        websocketUri =  new Uri(websocket[currentNodeName]);
-      }
-    }
-
-    currentNodeName = 'PJSUA_HTTPD_URI';
-    if (_validString(json, currentNodeName)) {
-      pjsuaHttpdUri = new Uri(json[currentNodeName]);
-    }
-
-    currentNodeName = 'System_Console';
-    if (_validMap(json, currentNodeName)) {
-      var systemConsole = json[currentNodeName];
-
-      currentNodeName = 'Max_Items';
-      if (_validInt(systemConsole, currentNodeName)) {
-        systemConsoleMaxItems = systemConsole[currentNodeName];
-      }
-    }
 
     currentNodeName = 'Event_Log';
     if (_validMap(json, currentNodeName)) {
@@ -220,10 +172,36 @@ class Configuration {
           break;
       }
     } else {
-      // Fallback.
       serverLogLevel = Level.INFO;
     }
 
+  }
+
+  bool _boolValue (Map configMap, String key, bool defaultValue) {
+    if ((configMap.containsKey(key)) && (configMap[key] is bool)) {
+      return configMap[key];
+    } else {
+      log.critical('Configuration parameter ${key} does not validate as bool');
+      return defaultValue;
+    }
+  }
+
+  int _intValue (Map configMap, String key, int defaultValue) {
+    if ((configMap.containsKey(key)) && (configMap[key] is int)) {
+      return configMap[key];
+    } else {
+      log.critical('Configuration parameter ${key} does not validate as int');
+      return defaultValue;
+    }
+  }
+
+  String _stringValue (Map configMap, String key, String defaultValue) {
+    if ((configMap.containsKey(key)) && (configMap[key] is String)) {
+      return (configMap[key].trim().length == 0) ? defaultValue : configMap[key];
+    } else {
+      log.critical('Configuration parameter ${key} does not validate as String');
+      return defaultValue;
+    }
   }
 
   bool _validBool (Map json, String node) {
@@ -235,14 +213,17 @@ class Configuration {
     return true;
   }
 
-  bool _validString (Map json, String node, {bool mayBeEmpty: false}) {
-    if (!_containsNode(json, node)) return false;
-    if (json[node] is! String) {
-      log.critical('Configuration node ${node} is not a String');
+  bool _validString (Map map, String key, {bool nonEmptyValueRequired : true}) {
+    if ((map.containsKey(key)) && (map[key] is String)) {
+      if ((map[key].trim().length == 0) && (nonEmptyValueRequired)) {
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      log.critical('Configuration parameter ${key} does not validate');
       return false;
     }
-    if (!mayBeEmpty && json[node].isEmpty) return false;
-    return true;
   }
 
   bool _validInt (Map json, String node, {int min, int max}){
@@ -263,12 +244,12 @@ class Configuration {
   }
 
   bool _validMap (Map json, String node) {
-    if (!_containsNode(json, node)) return false;
-    if (json[node] is! Map) {
+    if ((_containsNode(json, node)) && (json[node] is Map)) {
+      return true;
+    } else {
       log.critical('Configuration node ${node} is not a Map');
       return false;
     }
-    return true;
   }
 
   bool _containsNode (Map json, String node) {
