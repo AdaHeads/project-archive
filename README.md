eb3561bda78303778ebf6dd7ad0c448c74b12dfb
Jacob Sparre Andersen <jsa@adaheads.com>
2014-03-21 09:04:49 +0000

    Copying over build and test framework from 'Black'.  Created a single threaded example HTTP server.

--- README.md
+++ README.md
@@ -28,15 +28,16 @@ Dependencies
 - Black [2] - the library we're demonstrating here.
 - Ahven [3] - for unit testing.
 - GNAT [4] - since we use GNAT.Sockets to connect to a TCP/IP socket.
+- libesl [5] - as Black depends on it.
 
 
 Links
 -----
 
-If you want to find free Ada tools or libraries AdaIC [5] is an excellent
+If you want to find free Ada tools or libraries AdaIC [6] is an excellent
 starting point.  You can also take a look at the other source text
-repositories belonging to AdaHeads K/S [6] or those belonging to Jacob
-Sparre Andersen [7].
+repositories belonging to AdaHeads K/S [7] or those belonging to Jacob
+Sparre Andersen [8].
 
 1. Source text repository:
    https://github.com/AdaHeads/Black-examples
@@ -46,9 +47,12 @@ Sparre Andersen [7].
    http://www.ahven-framework.org/
 4. GNAT:
    http://libre.adacore.com/ - included in all well-assorted operating systems
-5. Free Ada Tools and Libraries:
+5. Black:
+   https://github.com/AdaHeads/libesl
+6. Free Ada Tools and Libraries:
    http://www.adaic.org/ada-resources/tools-libraries/
-6. Our repositories on GitHub:
+7. Our repositories on GitHub:
    https://github.com/AdaHeads
-7. Jacob Sparre Andersen's repositories on Bitbucket:
+8. Jacob Sparre Andersen's repositories on Bitbucket:
    http://repositories.jacob-sparre.dk/
+
---
+++ src/single_thread_server.adb
@@ -0,0 +1,58 @@
+with
+  Ada.IO_Exceptions;
+with
+  GNAT.Sockets;
+with
+  Black.Request,
+  Black.Response,
+  GNAT.Sockets.Convenience;
+
+procedure Single_Thread_Server is
+   use GNAT.Sockets;
+   Listener : Socket_Type;
+begin
+   Listener := Convenience.Make_Server (Port => 8080);
+
+   loop
+      declare
+         Connection : Socket_Type;
+         Client     : Sock_Addr_Type;
+      begin
+         Accept_Socket (Server  => Listener,
+                        Socket  => Connection,
+                        Address => Client);
+
+         declare
+            Request : constant Black.Request.Instance :=
+                        Black.Request.Parse_HTTP (Stream (Connection));
+            use Black.Response;
+         begin
+            if Request.Resource = "/redirect" then
+               Instance'Output
+                 (Stream (Connection),
+                  Redirect (Target    => "http://www.jacob-sparre.dk/",
+                            Permanent => False));
+            elsif Request.Resource = "/" then
+               Instance'Output
+                 (Stream (Connection),
+                  OK (Data => "You've visited the single threaded Black " &
+                              "example server."));
+            elsif Request.Resource = "/stop" then
+               Instance'Output
+                 (Stream (Connection),
+                  OK (Data => "You've visited the single threaded Black " &
+                              "example server.  Stopping..."));
+               exit;
+            else
+               Instance'Output (Stream (Connection),
+                                Not_Found (Resource => Request.Resource));
+            end if;
+         end;
+
+         Close_Socket (Socket => Connection);
+      exception
+         when Ada.IO_Exceptions.End_Error =>
+            null;
+      end;
+   end loop;
+end Single_Thread_Server;
