6c3e44475960a1bcde262dee77e19822b644584a
Jacob Sparre Andersen <jsa@adaheads.com>
2014-05-06 10:49:42 +0200

    Client: Save request too.

--- src/client.adb
+++ src/client.adb
@@ -15,7 +15,7 @@ procedure Client is
    use GNAT.Sockets;
    Connection : Socket_Type;
 begin
-   if Argument_Count in 3 .. 4 then
+   if Argument_Count in 3 .. 5 then
       declare
          Host     : String    renames Argument (1);
          Port     : Port_Type renames Port_Type'Value (Argument (2));
@@ -24,11 +24,30 @@ begin
          Connection := Convenience.Connect_To_Server (Host => Host,
                                                       Port => Port);
 
-         Black.Request.Instance'Output
-           (Stream (Connection),
-            Black.Request.Compose (Method   => Black.HTTP.Get,
-                                   Host     => Host,
-                                   Resource => Resource));
+         declare
+            use Ada.Streams.Stream_IO;
+            Request : constant Black.Request.Instance :=
+              Black.Request.Compose (Method   => Black.HTTP.Get,
+                                     Host     => Host,
+                                     Resource => Resource);
+            Copy    : File_Type;
+         begin
+            if Argument_Count < 5 then
+               Create (File => Copy,
+                       Name => "request.text");
+            else
+               Create (File => Copy,
+                       Name => Argument (5));
+            end if;
+
+            Black.Request.Instance'Output (Stream (Copy),
+                                           Request);
+
+            Close (File => Copy);
+
+            Black.Request.Instance'Output (Stream (Connection),
+                                           Request);
+         end;
 
          declare
             use Ada.Streams.Stream_IO;
@@ -36,10 +55,10 @@ begin
               Black.Response.Instance'Input (Stream (Connection));
             Output   : File_Type;
          begin
-            if Argument_Count = 3 then
+            if Argument_Count < 4 then
                Create (File => Output,
                        Name => "response.text");
-            elsif Argument_Count = 4 then
+            else
                Create (File => Output,
                        Name => Argument (4));
             end if;
